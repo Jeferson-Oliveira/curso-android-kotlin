1b5d8f38cecc4076e127efb7ecb5d52215fa8d07
Jeferson Oliveira <jefersonoliver7@gmail.com>
2018-03-23 17:41:03 -0300

    Aula ATM Finalização App

--- ATMConsultoria/app/src/main/java/com/example/jesus/jeferson/atmconsultoria/MainActivity.kt
+++ ATMConsultoria/app/src/main/java/com/example/jesus/jeferson/atmconsultoria/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.example.jesus.jeferson.atmconsultoria
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadComponents()
     }
+
+    private fun loadComponents() {
+        btnClientes.setOnClickListener {
+            val intent = Intent(this, DetalheClientesActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnEmpresa.setOnClickListener {
+            val intent = Intent(this, DetalheEmpresaActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnServicos.setOnClickListener {
+            val intent = Intent(this, DetalheServicosActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        btnContato.setOnClickListener {
+            val intent = Intent(this, DetalheContatoActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
 }
